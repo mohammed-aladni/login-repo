d8801e70a379553e84f962d9e7c04579f2216fe9
mohammed-aladni <mohammed8aladni@gmail.com>
2024-11-04 21:54:19 +0530

    modify to (forget password)

--- index.php
+++ index.php
@@ -63,7 +63,7 @@
           </div><br>
           
           <p class="recover">
-            <a href="recover_password.html">Recover Password</a>
+            <a href="recover_password.html">Forget the Password</a>
           </p>
           <div class="two-col">
           <div class="one">
